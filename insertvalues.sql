406c81edc91b71549524bf1abed2193eba95beae
Shijusekharan <shijusekharan@gmail.com>
2024-10-16 17:12:07 +0100

    Update insertvalues.sql

--- insertvalues.sql
+++ insertvalues.sql
@@ -1,4 +1,4 @@
 INSERT INTO shiju_tb1 (firstname, EmpID ) VALUES ('Adam',123);
-INSERT INTO shiju_tb1 (firstname, EmpID ) VALUES ('Meraj',234);
-INSERT INTO shiju_tb1 (firstname, EmpID ) VALUES ('Siva',355);
-INSERT INTO shiju_tb1 (firstname, EmpID ) VALUES ('Shiju',467);
+INSERT INTO shiju_tb2 (firstname, EmpID ) VALUES ('Meraj',234);
+INSERT INTO shiju_tb3 (firstname, EmpID ) VALUES ('Siva',355);
+INSERT INTO shiju_tb4 (firstname, EmpID ) VALUES ('Shiju',467);
