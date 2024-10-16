12907485e9df62c6e269ac8d15b7b4cb1e971f32
Shijusekharan <shijusekharan@gmail.com>
2024-10-16 13:09:47 +0100

    Create insertvalues.sql

---
+++ insertvalues.sql
@@ -0,0 +1,4 @@
+INSERT INTO shiju_tb1 (firstname, rollnumber) VALUES ('Raj',123);
+INSERT INTO shiju_tb1 (firstname, rollnumber) VALUES ('vijay',234);
+INSERT INTO shiju_tb1 (firstname, rollnumber) VALUES ('sekhar',355);
+INSERT INTO shiju_tb1 (firstname, rollnumber) VALUES ('lal',467);
