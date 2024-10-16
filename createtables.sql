90d821c30c3c9625800892b379ec6438f0b74a0b
Shijusekharan <shijusekharan@gmail.com>
2024-10-16 13:13:22 +0100

    Create createtables.sql

---
+++ createtables.sql
@@ -0,0 +1,4 @@
+CREATE TABLE shiju_tb1 ( firstname varchar(20), EmpID int(5));
+CREATE TABLE shiju_tb2 ( firstname varchar(20), EmpID int(5));
+CREATE TABLE shiju_tb3 ( firstname varchar(20), EmpID int(5));
+CREATE TABLE shiju_tb4 ( firstname varchar(20), EmpID int(5));
