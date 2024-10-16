3522e4e1a7d5791dad861ff67ebb69049d45ac9f
Shijusekharan <shijusekharan@gmail.com>
2024-10-16 17:11:39 +0100

    Update insertvalues.sql

--- insertvalues.sql
+++ insertvalues.sql
@@ -1,4 +1,4 @@
-INSERT INTO shiju_tb1 (firstname, rollnumber) VALUES ('Raj',123);
-INSERT INTO shiju_tb1 (firstname, rollnumber) VALUES ('vijay',234);
-INSERT INTO shiju_tb1 (firstname, rollnumber) VALUES ('sekhar',355);
-INSERT INTO shiju_tb1 (firstname, rollnumber) VALUES ('lal',467);
+INSERT INTO shiju_tb1 (firstname, EmpID ) VALUES ('Adam',123);
+INSERT INTO shiju_tb1 (firstname, EmpID ) VALUES ('Meraj',234);
+INSERT INTO shiju_tb1 (firstname, EmpID ) VALUES ('Siva',355);
+INSERT INTO shiju_tb1 (firstname, EmpID ) VALUES ('Shiju',467);
